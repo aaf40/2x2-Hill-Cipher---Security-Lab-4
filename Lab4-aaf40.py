fixed incorrect order output bug

Lab4-aaf40.py
@@ -43,17 +43,19 @@ def cipher_decryption(cipher, key):
 
     inv_det = pow(det, -1, 26)
 
-    cofactor_matrix = np.array([[ key_matrix[1, 1], -key_matrix[0, 1]], [-key_matrix[1, 0],  key_matrix[0, 0]]])
+    cofactor_matrix = np.array([
+        [key_matrix[1, 1], -key_matrix[0, 1]],
+        [-key_matrix[1, 0], key_matrix[0, 0]]
+    ])
     adjugate_matrix = cofactor_matrix
 
     inverse_key_matrix = (inv_det * adjugate_matrix) % 26
 
-    decrypted_numeric = (inverse_key_matrix @ cipher_matrix.T) % 26  
-    decrypted_text = ''.join(chr(num + ord('A')) for num in decrypted_numeric.flatten())  
+    decrypted_numeric = (inverse_key_matrix @ cipher_matrix.T) % 26
+    decrypted_text = ''.join(chr(num + ord('A')) for num in decrypted_numeric.T.flatten())
 
     print("Decrypted text:", decrypted_text)
     return decrypted_text
-
 
 def main():
     if len(sys.argv) != 3:
@@ -61,12 +63,12 @@ def main():
         sys.exit(1)
 
     # plain_text = sys.argv[1]
-    # encryption_key = sys.argv[2]
-    # cipher_encryption(plain_text, encryption_key)
+    # key = sys.argv[2]
+    # cipher_encryption(plain_text, key)
 
     cipher_text = sys.argv[1]
-    decryption_key = sys.argv[2]
-    cipher_decryption(cipher_text, decryption_key)
+    key = sys.argv[2]
+    cipher_decryption(cipher_text, key)
 
 if __name__ == "__main__":
     main()